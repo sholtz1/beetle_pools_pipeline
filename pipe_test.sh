f2367bcadc096319fd287968502d9f3c2655b945
Spencer Holtz <sholtz1@m199.cluster>
2020-10-02 19:00:27 -0600

    trial for recoving old file

--- pipe_test.sh
+++ pipe_test.sh
@@ -17,8 +17,10 @@ module load trimmomatic/0.36
 module load bwa/0.7.17
 module load samtools/1.9
 
-Forward=($( find *_R1_* ))
-Backward=($( find *_R2_* ))
+Forward=($( find /project/beetlegenes/2017BeetleData/ -name '*_R1_*'))
+Backward=($( find /project/beetlegenes/2017BeetleData/ -name '*_R2_*'))
+SctatchF=($( find  /gscratch/sholtz1/ -name '*_R1_*'))
+ScratchB=($( find  /gscratch/sholtz1/ -name '*_R2_*'))
 NumFiles=${#Forward[@]}
 
 for (( i=0; i<${NumFiles}; i++));
@@ -26,15 +28,11 @@ do
 
 # Trim the reads
 # Trim the reads
-trimmomatic PE -phred33 Pooled_samples/${Forward[$i]} Pooled_samples/${Backward[$i]} TestFiles/${Forward[$i]}_forward_paired.fq.gz TestFiles/${Forward[$i]}_forward_unpaired.fq.gz TestFiles/${Backward[$i]}_reverse_paired.fq.gz TestFiles/${Backward[$i]}_reverse_unpaired.fq.gz ILLUMINACLIP:NexteraPE-PE.fa:2:30:10 LEADING:20 TRAILING:20 SLIDINGWINDOW:4:15 MINLEN:100
+trimmomatic PE -phred33 ${Forward[$i]} ${Backward[$i]} /gscratch/sholtz1/${Forward[$i]}_forward_paired.fq.gz /gscratch/sholtz1/${Forward[$i]}_forward_unpaired.fq.gz /gscratch/sholtz1/${Backward[$i]}_reverse_paired.fq.gz /gscratch/sholtz1/${Backward[$i]}_reverse_unpaired.fq.gz ILLUMINACLIP:NexteraPE-PE.fa:2:30:10 LEADING:20 TRAILING:20 SLIDINGWINDOW:4:15 MINLEN:100
 
 # Align both the forward and backward reads to the reference genome
 # Align both the forward and backward reads to the reference genome
-bwa mem -t 30 Tribolium_ref_2020.fna  TestFiles/${Forward[$i]}_forward_paired.fq.gz TestFiles/${Backward[$i]}_reverse_paired.fq.gz > TestFiles/${Forward[$i]}.sam
+bwa mem -t 30 Tribolium_ref_2020.fna  /gscratch/sholtz1/${Forward[$i]}_forward_paired.fq.gz /gscratch/sholtz1/${Backward[$i]}_reverse_paired.fq.gz > TestFiles/${Forward[$i]}.sam
 
-# Sort it to make it a .sort file
-# Sort it to make it a .sort file
-samtools view -q 20 -b -S TestFiles/${Forward[$i]}.sam > TestFiles/${Forward[$i]}.temp.sam
-samtools sort -o TestFiles/${Forward[$i]}.sort TestFiles/${Forward[$i]}.temp.sam
 
 done
